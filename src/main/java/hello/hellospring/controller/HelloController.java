2da5a50237d442ef7c4143f7267a6edde60c301c
punsoo <ryxhdrnrmf@gmail.com>
2021-05-14 21:37:34 +0900

    feat: MemberService 구현

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,50 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.HelloSpringApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data","hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "pobi") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("pobi") String crong) {
+        Hello hello = new Hello();
+        hello.setDog("dogdog");
+        return hello;
+    }
+
+    static class Hello {
+        private String dog;
+
+        public String getDog() {
+            return dog;
+        }
+
+        public void setDog(String dog) {
+            this.dog = dog;
+        }
+    }
+}
